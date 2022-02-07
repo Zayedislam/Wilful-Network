7bbf968439b799508f67aca525ccf9c2f0b56a14
Zayedislam <islamzayed75@gmail.com>
2022-02-07 17:57:06 +0600

    deploy react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import React, { createContext, useState } from 'react';
+import {
+  BrowserRouter, Route, Routes
+} from "react-router-dom";
 import './App.css';
+import AddEvent from './Components/AddEvent/AddEvent';
+import Admin from './Components/Admin/Admin';
+//import Register from './Components/Register/Register';
+import Blog from './Components/Blog/Blog';
+import BodyArea from './Components/BodyArea/BodyArea';
+import Donation from './Components/Donation/Donation';
+import Login from './Components/Login/Login';
+import NotFound from './Components/NotFound/NotFound';
+import RegEvent from './Components/RegEvent/RegEvent';
+
+
+export const UserContext = createContext();
 
 function App() {
+  const [loggedInUser , setLoggedInUser] = useState({
+    name : '',
+    email : '',
+    date: '',
+    description:'',
+    img:''
+});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={[loggedInUser , setLoggedInUser]}>
+      <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<BodyArea />}/>
+        <Route path="/home" element={<BodyArea />}/>
+        <Route path="/RegEvent" element={<RegEvent />}/>
+        <Route path="/register" element={<Login/>}/>
+
+        <Route path="/blog" element={<Blog/>}/>
+        <Route path="/events" element={<RegEvent />}/>
+        <Route path="/donation" element={<Donation />}/>
+        <Route path="/admin" element={<Admin/>}/>
+        <Route path="/addevent" element={<AddEvent/>}/>
+        <Route path="*" element={<NotFound/>}/>
+
+
+        </Routes>
+      </BrowserRouter>
+
+    </UserContext.Provider>
+      
+    
   );
 }
 
---
+++ src/Components/Login/FirebaseConfig.js
@@ -0,0 +1,12 @@
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAoYZVIA5G1MCyi1IcD1UA7OE0i16gSoG0",
+  authDomain: "wilful-network.firebaseapp.com",
+  projectId: "wilful-network",
+  storageBucket: "wilful-network.appspot.com",
+  messagingSenderId: "906852810729",
+  appId: "1:906852810729:web:d68c8ba2ccc46ab5559578"
+};
+ export default firebaseConfig;
